f883a080f9cf7a64eca897c22d4da52eef22f417
mazhe <matthieu.volat@gmail.com>
2010-05-20 19:45:30 +0200

    Initial import.

---
+++ q3ut4_log_parser.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import re
+import sqlite3
+
+
+# Patterns
+frag_prog = re.compile(r"^[ ]*[0-9]+:[0-9]{2} Kill: [0-9]+ [0-9]+ [0-9]+: (.*) killed (.*) by (?!MOD_CHANGE_TEAM$|MOD_FALLING$|UT_MOD_BLED$|UT_MOD_FLAG$)(.*)$")
+fall_prog = re.compile(r"^[ ]*[0-9]+:[0-9]{2} Kill: [0-9]+ [0-9]+ [0-9]+: <non-client> killed (.*) by MOD_FALLING$")
+bled_prog = re.compile(r"^[ ]*[0-9]+:[0-9]{2} Kill: [0-9]+ [0-9]+ [0-9]+: <non-client> killed (.*) by UT_MOD_BLED$")
+
+# Database connection
+db_conn = None
+
+
+# Create db
+def create_db():
+	global db_conn
+	db_conn = sqlite3.connect(':memory:')
+	db_conn.execute('create table frags (fragger text, fragged text)')
+	db_conn.commit()
+
+
+# Read the log and populate db
+def parse_log(logpath):
+	global db_conn
+	logf = open(logpath, 'r')
+	while 1:
+		logline = logf.readline()
+		if (not logline):
+			break
+
+		m = frag_prog.match(logline)
+		if (m != None):
+			# Update the frags
+			frag_tuple = (m.group(1), m.group(2))
+			db_conn.execute(
+					'''insert into frags values (?, ?)''', 
+					frag_tuple)
+	db_conn.commit()
+	logf.close()
+
+
+# 
+def frags_repartition():
+	global db_conn
+	print "    <a name=\"1\"><h2>Frags repartition per player</h2></a>"
+	curs = db_conn.cursor()
+	curs.execute('''
+select fragger, fragged, count(*) as frags 
+from frags 
+group by fragger, fragged 
+order by lower(fragger) asc, count(*) desc
+''')
+	player = None
+	for row in curs:
+		if (player != row[0]):
+			if (player):
+				print "    </table>"
+			print """\
+    <h3>%s fragged:</h3>
+    <table>\
+""" % row[0]
+			player = row[0]
+
+		print """\
+      <tr>
+        <td style="width: 180px;">%s</td>
+        <td><span style="background: #EFA21E;">\
+""" % row[1],
+		for i in xrange(0, row[2]):
+			print "&nbsp;",
+		print """</span>&nbsp;%s</td>
+      </tr>\
+""" % row[2]
+	print "    </table>"
+
+
+# 
+def death_repartition():
+	global db_conn
+	print """\
+    <a name=\"2\"><h2>Deaths repartition per player</h2></a>
+    <table>
+"""
+	curs = db_conn.cursor()
+	curs.execute('''
+select fragged, fragger, count(*) 
+from frags 
+group by fragged, fragger
+order by lower(fragged) asc, count(*) desc
+''')
+	player = None
+	for row in curs:
+		if (player != row[0]):
+			if (player):
+				print "    </table>"
+			print """\
+    <h3>%s has been fragged by:</h3>
+    <table>\
+""" % row[0]
+			player = row[0]
+
+		print """\
+      <tr>
+        <td style="width: 180px;">%s</td>
+        <td><span style="background: #EFA21E;">\
+""" % row[1],
+		for i in xrange(0, row[2]):
+			print "&nbsp;",
+		print """</span>&nbsp;%s</td>
+      </tr>\
+""" % row[2]
+	print "    </table>"
+
+
+#
+def frag_ranking():
+	global db_conn
+	print """\
+    <a name="#3"><h2>Frag-based ranking</h2></a>
+    <ol>\
+"""
+	curs = db_conn.cursor()
+	curs.execute('''
+select fragger, count(*) as frags 
+from frags 
+where fragger != fragged
+group by fragger
+order by count(*) desc, lower(fragger) asc
+''')
+	for row in curs:
+		print "      <li>%s (%s)</li>" % (row[0], row[1])
+	print "    </ol>"
+
+
+#
+def fdratio_ranking():
+	global db_conn
+	print """\
+    <a name="#4"><h2>Frag/death ratio-based ranking</h2></a>
+    <ol>\
+"""
+	players_curs = db_conn.cursor()
+	players_curs.execute('''
+select fragger
+from frags
+group by fragger
+''')
+
+	ratios = []
+	for players_row in players_curs:
+		tuple = (players_row[0],)
+
+		frags_curs = db_conn.cursor()
+		frags_curs.execute('''
+select count(*)
+from frags
+where fragger = ?
+	and fragger != fragged
+''', tuple)
+		frags_row = frags_curs.fetchone()
+
+		deaths_curs = db_conn.cursor()
+		deaths_curs.execute('''
+select count(*)
+from frags
+where fragged = ?
+''', tuple)
+		deaths_row = deaths_curs.fetchone()
+
+		ratios.append((players_row[0], float(frags_row[0]) / float(deaths_row[0])))
+
+	ratios.sort(key=lambda ratio: ratio[1], reverse=True)
+	for r in ratios:
+		print "      <li>%s (%f)</li>" % (r[0], r[1])
+
+	print "    </ol>"
+
+
+# Main function
+def main():
+	global db_conn
+
+	if (len(sys.argv) < 2):
+		sys.exit(1)
+
+	create_db()
+
+	if os.path.isdir(sys.argv[1]):
+		for logrpath in os.listdir(sys.argv[1]):
+			logfpath = ''.join([sys.argv[1], '/', logrpath])
+			parse_log(logfpath)
+	else:
+		parse_log(sys.argv[1])
+
+	print """\
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en-US">
+  <head>
+    <title>UrbT stats page</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <style>
+      h2 {
+        border-bottom: 1px solid grey;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Urban Terror statistics webpage</h1>
+    <hr>
+    <ul>Available stats:
+      <li><a href="#1">Frags repartition per player</a></li>
+      <li><a href="#2">Deaths repartition per player</a></li>
+      <li><a href="#3">Frag-based ranking</a></li>
+      <li><a href="#4">Frags/Deaths ratio-based ranking</a></li>
+    </ul>\
+"""
+
+	frags_repartition()
+	death_repartition()
+	frag_ranking()
+	fdratio_ranking()
+
+	db_conn.close()
+
+	print """\
+    <hr>
+  </body>
+</html>\
+"""
+
+
+if __name__ == '__main__':
+	main()
